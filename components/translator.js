db8c53a1cf4a3a38d58463adbaf473580334aaf6
u210979 <smadar.klopshtok@sbb.ch>
2025-04-28 11:50:21 +0200

    All american to british tests are green

--- components/translator.js
+++ components/translator.js
@@ -1,9 +1,107 @@
+const { dot } = require('mocha/lib/reporters/index.js');
 const americanOnly = require('./american-only.js');
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require('./british-only.js');
+const { text } = require('body-parser');
+
+
+
+
 
 class Translator {
+
+    constructor() {
+        this.allAmericanToBritish = { ...americanToBritishSpelling, ...americanOnly, ...americanToBritishTitles };
+        this.britishToAmericanTitles = { ...this.invertDictionary(americanToBritishTitles) };
+        this.allBritishToAmerican = { ...this.invertDictionary(americanToBritishSpelling), ...britishOnly, ...this.britishToAmericanTitles };
+    }
+
+    invertDictionary(dict) {
+        let invertedDictionary = {};
+        for (let key in dict) {
+            invertedDictionary[dict[key]] = key
+        }
+        return invertedDictionary;
+    }
+
+    correctTitleCapitalization(wordToTranslate, translatedWord, titlesDictionary) {
+        if (titlesDictionary[wordToTranslate.toLowerCase()]) {
+            return translatedWord.charAt(0).toUpperCase() + translatedWord.slice(1);
+        }
+        return translatedWord;
+    }
+
+    doTranslate(wordsToTranslate, translatedWords, dictionary, titlesDictionary) {
+        //console.log(`textToTranslate: ${wordsToTranslate.join(' ')}, partiallyTranslatedText: ${translatedWords.join(' ')}`);
+
+        if (wordsToTranslate.length === 1) {
+            if (dictionary[wordsToTranslate[0].toLowerCase()]) {
+                translatedWords.push(this.correctTitleCapitalization(wordsToTranslate[0], dictionary[wordsToTranslate[0]], titlesDictionary));
+            } else {
+                translatedWords.push(this.correctTitleCapitalization(wordsToTranslate[0], wordsToTranslate[0], titlesDictionary));
+            }
+            return translatedWords;
+        }
+
+        for (let startIndex = 0; startIndex < wordsToTranslate.length; startIndex++) {
+            for (let endIndex = wordsToTranslate.length; endIndex >= 0; endIndex--) {
+                const currentWords = wordsToTranslate.slice(startIndex, endIndex);
+                const currentTextToTranslate = currentWords.join('');
+                const currentTranslation = dictionary[currentTextToTranslate.toLowerCase()];
+                if (currentTranslation) {
+                    //console.log(`Found translation: toTranslate=${currentTextToTranslate}   translation=${currentTranslation}`);
+                    translatedWords.push(this.correctTitleCapitalization(currentTextToTranslate, currentTranslation, titlesDictionary));
+                    return this.doTranslate(wordsToTranslate.slice(endIndex, wordsToTranslate.length), translatedWords, dictionary, titlesDictionary);
+                }
+                //console.log(`no translation for: ${currentTextToTranslate}`);
+            }
+            translatedWords.push(wordsToTranslate[startIndex]);
+        }
+        return translatedWords;
+    }
+
+    isTimeString(str) {
+        const regexp = /^(2[0-3]|[01]?[0-9])[:.]([0-5]?[0-9])$/;
+        console.log(`str: ${str}  result: ${regexp.test(str)}`);
+        return regexp.test(str);
+    }
+
+    translateTimeString(words, translationLocale) {
+        const newWords = [];
+
+        words.forEach(word => {
+            console.log(`word: ${word}`);
+            if (this.isTimeString(word)) {
+                switch (translationLocale) {
+                    case 'american-to-british':
+                        newWords.push(word.replaceAll(':', '.'));
+                        break;
+                    case 'british-to-american':
+                        newWords.push(word.replaceAll('.', ':'));
+                        break;
+                    default:
+                        newWords.push(word);
+                }
+            } else {
+                newWords.push(word);
+            }
+        });
+        return newWords;
+    }
+
+    translate(textToTranslate, translationLocale) {
+        let wordsToTranslate = textToTranslate.split(/(?=[\s,.?])|(?<=[\s,.?])/g);
+        wordsToTranslate = this.translateTimeString(wordsToTranslate, translationLocale);
+        switch (translationLocale) {
+            case 'american-to-british':
+                return this.doTranslate(wordsToTranslate, [], this.allAmericanToBritish, americanToBritishTitles).join('');
+            case 'british-to-american':
+                return this.doTranslate(wordsToTranslate, [], this.allBritishToAmerican, this.britishToAmericanTitles).join('');
+            default:
+                return textToTranslate;
+        }
+    }
 
 }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,65 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Translator = require('../components/translator.js');
+const translator = new Translator();
 
 suite('Unit Tests', () => {
+    suite('American -> British', () => {
+        test('Translate Mangoes are my favorite fruit. to British English', () => {
+            const americanSpelling = 'Mangoes are my favorite fruit.';
+            const expectedResult = 'Mangoes are my favourite fruit.';
+            assert.equal(translator.translate(americanSpelling, 'american-to-british'), expectedResult);
+        });
+         test('Translate I ate yogurt for breakfast. to British English', () => {
+            const americanSpelling = 'I ate yogurt for breakfast.';
+            const expectedResult = 'I ate yoghurt for breakfast.';
+            assert.equal(translator.translate(americanSpelling, 'american-to-british'), expectedResult);
+        });
+        test('Translate We had a party at my friend\'s condo. to British English', () => {
+            const americanSpelling = 'We had a party at my friend\'s condo.';
+            const expectedResult = 'We had a party at my friend\'s flat.';
+            assert.equal(translator.translate(americanSpelling, 'american-to-british'), expectedResult);
+        });
+        test('Translate Can you toss this in the trashcan for me? to British English', () => {
+            const americanSpelling = 'Can you toss this in the trashcan for me?';
+            const expectedResult = 'Can you toss this in the bin for me?';
+            assert.equal(translator.translate(americanSpelling, 'american-to-british'), expectedResult);
+        });
+       test('Translate The parking lot was full. to British English', () => {
+            const americanSpelling = 'The parking lot was full.';
+            const expectedResult = 'The car park was full.';
+            assert.equal(translator.translate(americanSpelling, 'american-to-british'), expectedResult);
+        });
+        test('Translate Like a high tech Rube Goldberg machine. to British English', () => {
+            const americanSpelling = 'Like a high tech Rube Goldberg machine.';
+            const expectedResult = 'Like a high tech Heath Robinson device.';
+            assert.equal(translator.translate(americanSpelling, 'american-to-british'), expectedResult);
+        });
+        test('Translate To play hooky means to skip class or work. to British English', () => {
+            const americanSpelling = 'To play hooky means to skip class or work.';
+            const expectedResult = 'To bunk off means to skip class or work.';
+            assert.equal(translator.translate(americanSpelling, 'american-to-british'), expectedResult);
+        });
+        test('Translate No Mr. Bond, I expect you to die. to British English', () => {
+            const americanSpelling = 'No Mr. Bond, I expect you to die.';
+            const expectedResult = 'No Mr Bond, I expect you to die.';
+            assert.equal(translator.translate(americanSpelling, 'american-to-british'), expectedResult);
+        });
+        test('Translate Dr. Grosh will see you now. to British English', () => {
+            const americanSpelling = 'Dr. Grosh will see you now.';
+            const expectedResult = 'Dr Grosh will see you now.';
+            assert.equal(translator.translate(americanSpelling, 'american-to-british'), expectedResult);
+        });
+        test('Translate Lunch is at 12:15 today. to British English', () => {
+            const americanSpelling = 'Lunch is at 12:15 today.';
+            const expectedResult = 'Lunch is at 12.15 today.';
+            assert.equal(translator.translate(americanSpelling, 'american-to-british'), expectedResult);
+        });
+    });
+    suite('British -> American', () => {
 
+    });
+    suite('Highlight translation', () => {
+
+    });
 });
